ea7567723ef21ea852eaf873b82c973d03dd08ac
Dominick McKoy <dominick.mckoy@gmail.com>
2018-04-05 21:16:46 -0400

    Pseudocoded user all four user stories

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 'use strict';
-function check$() {
+function check$(){
   console.log('jQuery, the notorious $, is available for use.');
 }
-function main() {
-  check$();
+function renderShoppingList(){
+  // shopping list should be rendered to the page
+  console.log('`renderShoppingList` ran like a charm.');
 }
 
-$(main);
+function addItem(){
+  // an item provided by user should be added to shopping list
+  console.log('`addItem` ran like a charm.');
+}
+
+function checkItem(){
+  // an item (with button, 'check') should be able to be checked
+  console.log('`checkItem` ran like a charm');
+}
+
+function deleteItem(){
+  // an item (with button, 'delete') should be able to be deleted
+  console.log('`deleteItem` ran like a charm');
+}
+
+function handleShoppingList(){
+  check$();
+  renderShoppingList();
+  addItem();
+  checkItem();
+  deleteItem();
+}
+
+$(handleShoppingList);
