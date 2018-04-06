48f3c7079b26dff355347e315de554ffbe95992f
Dominick McKoy <dominick.mckoy@gmail.com>
2018-04-05 21:35:13 -0400

    Rendered hardcoded shopping list to DOM.

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ const STORE = [
 function check$(){
   console.log('jQuery, the notorious $, is available for use.');
 }
+
+function createDOMList(arr) {
+  return `
+  <li>${arr[0].name}</li>
+  <li>${arr[1].name}</li>
+  <li>${arr[2].name}</li>
+  <li>${arr[3].name}</li>
+  `;
+}
 function renderShoppingList(){
   // shopping list should be rendered to the page
   console.log('`renderShoppingList` ran like a charm.');
+  const DOMList = createDOMList(STORE);
+  $('.js-shopping-list').html(DOMList);
 }
 
 function addItem(){
