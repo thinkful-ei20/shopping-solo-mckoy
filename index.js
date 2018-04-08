5e47499e59bf86a81da016d4073cda6aca96c767
Dominick McKoy <dominick.mckoy@gmail.com>
2018-04-07 23:25:37 -0400

    Refactored some functions and added states to the STORE database

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 'use strict';
 // USER STORY 1: RENDER SHOPPING LIST TO DOM
-// creates a startup shopping list
+// Creates a database to store shopping list and 
+// methods to call on the shopping list.
 const STORE = {
-  testKey: [{name: 'Button (or whatever) works', checked: false}],
+  stateOfStore: [
+    {name: 'unsortedShoppingList', state: false},
+    {name: 'filterChecked', state: false},
+    {name: 'filterAlpha', state: false},
+    {name: 'editChecked', state: false},
+    {name: 'advancedDisplayed', state: false}
+  ],
+  // Stores an start-up shopping list of items, 
+  // each with a item name and whether or not this item has been checked.
+  // New items added in the DOM are stored in this list. 
   shoppingList: [
     {name: 'apples', checked: false},
     {name: 'oranges', checked: false},
     {name: 'milk', checked: true},
     {name: 'bread', checked: false}],
+  // Stores a list of shopping list items deleted in the DOM,
+  // starting with an empty list.
   deletedItemsList: [],
-  filterChecked: function(){
-    return this.shoppingList.filter(item => !item.checked);
-  },
-  filterAlpha: function(){
-    return this.shoppingList.slice().sort((a,b) => a.name>b.name);
-  },
   filterDeleted: () => {},
-  filterByName: function(name){
-    return this.shoppingList.filter(item => item.name.includes(name));
-  } 
+  // Filters items in the shopping list 
+  // by whether or not this item has been checked.
+  filterChecked: function(){return this.shoppingList.filter(item => !item.checked);},
+  // Filters items in the shopping list by making a copy,
+  // sorting that copy in place, and returning the sorted copy.
+  filterAlpha: function(){return this.shoppingList.slice().sort((a,b) => a.name>b.name);},
+  // Filters items in the shopping list by a search item,
+  // passed as the `name` parameter.
+  filterByName: function(name){return this.shoppingList.filter(item => item.name.includes(name));},
+  searchName: ''
 };
 // Creates an li element with item's name, buttons, classes, and attributes dubbed DOMItem.
 const createDOMItem = (item, index) => `
@@ -36,7 +49,7 @@ const createDOMItem = (item, index) => `
       </button>
     </div>
   </li>`;
-
+// Creates an input form for editing, buttons, classes, and attributes dubbed DOMEditItem.
 const createDOMEditItem = () => `
   <span class="shopping-item js-shopping-item">
     <form id="js-shopping-list-form">
@@ -54,76 +67,103 @@ const createDOMEditItem = () => `
       <span class="button-label">submit change</span>
     </button>
   </div>`;
-// Maps through the shopping list and creates a DOMItem for each item.
+// Maps through the a given array (such as the shopping list) and creates a DOMItem for each item.
 const createDOMList = arr => arr.map((item, index) => createDOMItem(item, index)).join('');
-/*
-const createEditDOMList = arr => arr.map((item, index) => 
-  (index===0) ? createDOMEditItem(item, index) : createDOMItem(item, index));
-*/
-// Renders shopping list to the DOM using `createDOMList`.
+// Renders an array (such as the shopping list) to the DOM using `createDOMList`.
 const renderShoppingList= arr => { $('.js-shopping-list').html(createDOMList(arr));};
 
 // USER STORY 2: ADD NEW SHOPPING LIST ITEM
-// Gets new shopping list item submitted by user.
-const getNewItem = (itemLoc) => $(itemLoc).val();
-// Empties the submit form.
-const emptyForm = () => { $('.js-shopping-list-entry').val('');};
-// Adds new item to shopping list.
+// Returns the value at a given location in the DOM
+const getNewItem = (itemLoc) => $(itemLoc).val().toLowerCase();
+// Empties the value at a given location in the DOM.
+const clearValue = (itemLoc) => { $(itemLoc).val('');};
+// Gets value of `getNewItem` and adds it to a given database with
+// a 'checked' key initialized to false.
 const addItemToShoppingList = database => { database.push({name: getNewItem('.js-shopping-list-entry'), checked: false}); };
-// When clicking submit, an item provided by user is added to the shopping list and rendered to the DOM.
+// When clicking submit, an item provided by user is added to the shopping list,
+// rendered to the DOM, then cleared from the 'add new item' field.
 const handleAddingItems = () => {
   $('#js-shopping-list-form').submit( (event) => {
     event.preventDefault();
     addItemToShoppingList(STORE.shoppingList);
     renderShoppingList(STORE.shoppingList);
-    emptyForm();
+    clearValue('.js-shopping-list-entry');
   });
 };
-const getEditItem = () => $('.js-edit-list-entry').val();
-
 
 // USER STORY 3 & 4: CHECK OR DELETE ITEMS
-// Returns the index of a clicked item
-const findIndexOfItem = eventLoc => $(eventLoc).closest('.js-item-index-element').data('itemIndex');
-// Listen for clicks on check or delete button then re-renders the shopping list to the DOM
+// Returns the index of a item given its location.
+const findIndexOfItem = location => $(location).closest('.js-item-index-element').data('itemIndex');
+// Listens for clicks on given button in a given location,
+// and if there is a given callback function, passes the given list to render and
+// and an item index, then renders a shopping list according to the given list to render.
+// If there is no callback function, renders a shopping list according to the given list to render. 
 const checkWhichButton = (buttonLoc, typeOfButton, listToRender, callbackFn) => {
   $(buttonLoc).on('click', typeOfButton, (event) => { 
     if(callbackFn){
-      callbackFn(listToRender, findIndexOfItem(event.target));
-      renderShoppingList(listToRender);
+      callbackFn(listToRender);
     } else { renderShoppingList(listToRender); }
   });
 };
-// If user clicks check button, item is striked or unstriked.
-const handleCheckingItems = () => checkWhichButton('.js-shopping-list', '.js-item-toggle', STORE.shoppingList, (database, index) => 
-  database[index].checked = !database[index].checked);
-// If user clicks delete button, delete the item.
-const handleDeletingItems = () => checkWhichButton('.js-shopping-list', '.js-item-delete', STORE.shoppingList, (database, index) => {
-  STORE.deletedItemsList.push(database.slice().splice(index, 1)[0]);
-  return database.splice(index, 1);
+// When clicking `check` button, the corresponding shopping list item is striked/unstriked.
+const handleCheckingItems = () => checkWhichButton('.js-shopping-list', '.js-item-toggle', STORE.shoppingList, (database) => {
+  database[findIndexOfItem(event.target)].checked = !database[findIndexOfItem(event.target)].checked;
+  renderShoppingList(database);
+});
+// When clicking `delete` button, the corresponding shopping list item is deleted and
+// added to the deleted items list in the database.
+const handleDeletingItems = () => checkWhichButton('.js-shopping-list', '.js-item-delete', STORE.shoppingList, (database) => {
+  STORE.deletedItemsList.push(database.slice().splice(findIndexOfItem(event.target), 1)[0]);
+  database.splice(findIndexOfItem(event.target), 1);
+  renderShoppingList(database);
 });
 
 // USER STORY 5: FILTER BUTTONS
-// Replaces current given class (in dot form) which a new given class (no dot) and a new text message.
+// Replaces current given class (in dot form) with a new given class (no dot) and a new text message.
 const replaceClassAndText = (currDotClass, newNoDotClass, text) => {
   $(currDotClass).text(text);
   $(currDotClass).removeClass().addClass(newNoDotClass);
 };
 
-// Handles 'Show unchecked items'/'Show all items' button
+/*
+// REFACTOR TO USE `checkWhichButton` FUNCTION FOR HANDLING FILTERING UNCHECKED ITEMS
+const handleFilteringUncheckedItems = () => {
+  const buttonText = $('.js-filter-unchecked').find('.button-label').text();
+  checkWhichButton('.js-buttons', '.js-filter-unchecked', STORE.filterChecked(), (database) => {
+    if (buttonText==='Show unchecked items') {
+      console.log(database);
+      renderShoppingList(database);
+      replaceClassAndText('.js-filter-unchecked', 'js-filter-checked', 'Show all items');
+    } else {
+      renderShoppingList(STORE.shoppingList);
+    }
+  });
+  checkWhichButton('.js-buttons', '.js-filter-checked', STORE.shoppingList, database => {
+    $('.js-advanced-forms').remove();
+    renderShoppingList(database);
+    replaceClassAndText('.js-filter-checked', 'js-filter-unchecked', 'Show unchecked items');
+  });
+};
+*/
+
+// Handles 'Show unchecked items'/'Show all items' button.
 const handleFilteringUncheckedItems= () => {
   const buttonText = $('.js-filter-unchecked').find('.button-label').text();
   $('.js-buttons').on('click', '.js-filter-unchecked', () => {
-    // If user clicks on the 'Show unchecked items', a shopping list filtered by unchecked renders to the DOM and the button will now say 'Show all items' 
+    // If user clicks on the 'Show unchecked items', a shopping list filtered by unchecked 
+    // renders to the DOM and the button will now say 'Show all items'.
     if (buttonText==='Show unchecked items') {
       renderShoppingList(STORE.filterChecked());
       replaceClassAndText('.js-filter-unchecked', 'js-filter-checked', 'Show all items');
     } else {
-      // If user clicks on the 'Show items in original order', the original shopping list renders to the DOM and the button will now say 'Show unchecked items'
+      // If user clicks on the 'Show items in original order', the original shopping list renders to the DOM 
+      // and the button will now say 'Show unchecked items'.
       renderShoppingList(STORE.shoppingList);
     }
   });
-  // If user clicks on the 'Show all items', the entire shopping list renders to the DOM and the button will now say 'Show all items'
+  // If user clicks on the 'Show all items', 
+  // the entire shopping list renders to the DOM and 
+  // the button will now say 'Show all items'
   $('.js-buttons').on('click', '.js-filter-checked', () => { 
     $('.js-advanced-forms').remove();
     renderShoppingList(STORE.shoppingList);
@@ -131,7 +171,7 @@ const handleFilteringUncheckedItems= () => {
   });
 };
 
-// Creates two radio buttons and an input box for submitting searches
+// Creates two radio buttons and an input box for submitting searches.
 const createAdvancedDOMForm = () => `
   <div class="js-advanced-forms">
     <form id="js-item-sort-form">
@@ -188,8 +228,9 @@ const handleSortAlphaDeletedAndSearch = () => {
     $('.js-advanced-checked').find('.button-label').text('Show advanced options');
     $('.js-advanced-checked').removeClass('js-advanced-checked').addClass('js-advanced-unchecked');
   });
+  // When user types into the search field
   $('.js-buttons').on('keyup', '.js-item-search-entry', event => {
-    renderShoppingList(STORE.filterByName($(event.target).val()));
+    renderShoppingList(STORE.filterByName($(event.target).val().toLowerCase()));
   });
 };
 
@@ -203,11 +244,6 @@ const handleEditingItems = () => {
   });
 };
 
-const handleSearchingItems = () => {
-
-};
-
-
 // Handles all USER STORIES.
 const handleShoppingList = () => {
   renderShoppingList(STORE.shoppingList);
@@ -218,8 +254,6 @@ const handleShoppingList = () => {
   handleCheckingItems();
   handleDeletingItems();
   handleEditingItems();
-  handleSearchingItems();
 };
-
 // call handler when the DOM is ready
 $(handleShoppingList);
